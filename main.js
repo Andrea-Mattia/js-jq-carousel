1ebd8bd6c65862d253cc153f6881cc717e886f28
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-04-08 18:16:22 +0200

    workingjs and jQuery

---
+++ main.js
@@ -0,0 +1,21 @@
+/**
+ * Descrizione
+ * Creare uno slider di immagini come visto questa mattina a lezione.
+ * Potete usare le immagini che desiderate. Per facilitarvi la vita usate immagini delle stesse dimensioni :faccia_leggermente_sorridente:
+ * Lo slider prevederà due frecce (icone) che permettono di scorrere tra le immagini dello slider.
+ * Inoltre per scorrere le immagini permettere anche l’uso delle frecce sinistra e destra della tastiera.
+ * Utilizziamo una classe first e last per capire quali sono la prima e ultima immagine dello slider.
+ * Utilizziamo una classe active per aiutarci a capire quale è l’immagine attuale da visualizzare nello slider.
+ * 
+ * 
+ * Bonus
+ * Clicchiamo sui pallini e mostriamo l’immagine corrispondente
+ */
+
+$(document).ready(function() {
+
+    console.log('Works');
+    console.log('jQuery', $);
+
+    // End doc ready
+});
